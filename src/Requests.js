e0246f2ddfea15f5f5d17a12e544eadca9e01bf1
jessroque <jessroque16@gmail.com>
2022-11-03 15:47:25 +0800

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "fa217ec224ad366a40a8cfbcb7540b9a";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
